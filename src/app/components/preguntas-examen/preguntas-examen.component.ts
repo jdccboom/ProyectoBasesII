f4307ebba76bd2c79ca78475c09fa9e00754a77e
jdccboom <juand.cortesc1@uqvirtual.edu.co>
2024-05-29 12:48:40 -0500

    Se creo encarpetado preguntas-examen

---
+++ src/app/components/preguntas-examen/preguntas-examen.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-preguntas-examen',
+  standalone: true,
+  imports: [],
+  templateUrl: './preguntas-examen.component.html',
+  styleUrl: './preguntas-examen.component.css'
+})
+export class PreguntasExamenComponent {
+
+}
